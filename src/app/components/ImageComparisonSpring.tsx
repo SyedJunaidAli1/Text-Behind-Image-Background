25425d57fce464089178d14086a446e7c7db9a45
SyedJunaidAli1 <syedjunaidali790@gmail.com>
2026-01-27 15:34:56 +0530

    ImageComparison moition animation added

--- src/app/components/ImageComparisonSpring.tsx
+++ src/app/components/ImageComparisonSpring.tsx
@@ -1,12 +1,36 @@
-'use client'
-import React from 'react';
-import { ImageComparison, ImageComparisonImage, ImageComparisonSlider } from '../../../components/motion-primitives/image-comparison';
+"use client";
+import {
+  ImageComparison,
+  ImageComparisonImage,
+  ImageComparisonSlider,
+} from "../../../components/motion-primitives/image-comparison";
+import { motion } from "motion/react";
+
+const fadeUp = {
+  hidden: { opacity: 0, y: 30 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.6, ease: "easeOut" },
+  },
+};
 
 const ImageComparisonSpring = () => {
   return (
-    <div className="flex items-center justify-center w-full min-h-[300px] px-4">
+    <motion.div
+      variants={fadeUp}
+      initial="hidden"
+      visible={{
+        opacity: 1,
+        y: 0,
+        transition: { duration: 0.6, ease: "easeIn" },
+      }}
+      whileInView="visible"
+      viewport={{ once: true }}
+      className="flex items-center justify-center w-full min-h-[300px] px-4"
+    >
       <ImageComparison
-        className="relative w-full max-w-4xl aspect-[16/10] h-auto min-h-[200px] my-2 rounded-lg border border-zinc-200 dark:border-zinc-800"
+        className="relative w-full max-w-4xl aspect-[16/10] h-auto min-h-[200px] my-2 rounded-lg border"
         enableHover
         springOptions={{
           bounce: 0.3,
@@ -24,11 +48,9 @@ const ImageComparisonSpring = () => {
           position="right"
           className="object-cover w-full h-full"
         />
-        <ImageComparisonSlider 
-          className="w-0.5 bg-white/30 backdrop-blur-xs" 
-        />
+        <ImageComparisonSlider className="w-0.5 bg-white/30 backdrop-blur-xs" />
       </ImageComparison>
-    </div>
+    </motion.div>
   );
 };
 
